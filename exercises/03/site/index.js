0e3e6914270465c5f77f55ebedf56dce74c265fa
Yaroslav Ternovskiy <yaroslavternovskiy@Yaroslavs-MacBook-Pro.local>
2020-10-23 17:34:33 +0300

    Prevent man-in-the-middle attack

--- exercises/03/site/index.js
+++ exercises/03/site/index.js
@@ -1,5 +1,6 @@
 const localHost = require("https-localhost");
 const express = require("express");
+const helmet = require("helmet");
 const session = require("express-session");
 const routeLogin = require("./routes/login");
 const routeMessages = require("./routes/messages");
@@ -9,6 +10,10 @@ const domain = "localhost.charlesproxy.com";
 
 const app = localHost(domain);
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet.hsts({
+    maxAge: 60 * 60 * 24 * 365,
+    includeSubDomains: true,
+}))
 
 app.use(
   session({
@@ -27,6 +32,14 @@ routeMessages(app);
 app.use("/static", express.static(__dirname + "/static"));
 
 app.listen(port);
+
+const appRedir = express();
+
+appRedir.use((req, resp) => {
+    resp.redirect(`https://${domain}${req.url}`);
+})
+
+appRedir.listen(80)
 
 console.log(
   `open https://${domain} to observe localhost network traffic via Charles`

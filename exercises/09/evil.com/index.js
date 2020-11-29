f3e404bf230cc9f6bb24fcec1949268b7adec6c4
Yaroslav Ternovskiy <yaroslavternovskiy@Yaroslavs-MacBook-Pro.local>
2020-11-29 15:56:52 +0300

    imitate xss attack

--- exercises/09/evil.com/index.js
+++ exercises/09/evil.com/index.js
@@ -4,4 +4,9 @@ const app = localHost("evil.com");
 
 app.use(express.static(__dirname + "/static"));
 
+app.route('/hijack').get(((req, res) => {
+    console.log(req.query.payload);
+    res.send('ok');
+}))
+
 app.listen(666);
